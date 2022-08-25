287edad49430d9d5bc1ef99345127bfdf75c0ce1
borisavila539 <borisavila539@gmail.com>
2022-08-25 14:31:15 -0600

    cambio del tamano de la letra

--- src/Components/constant.js
+++ src/Components/constant.js
@@ -1,17 +1,17 @@
 import { PixelRatio } from "react-native";
 
-const IconosBottomTab = parseInt(PixelRatio.getFontScale()*32.61);
-const IconSelect = parseInt(PixelRatio.getFontScale()*21.74);
-const IconCamera = parseInt(PixelRatio.getFontScale()*54.35);
-const IconHeader = parseInt(PixelRatio.getFontScale()*43.48);
-const LabelBottomTab = parseInt(PixelRatio.getFontScale()*13.04);
-const TextoPantallas = parseInt(PixelRatio.getFontScale()*17.39);
-const TextoHeader = parseInt(PixelRatio.getFontScale()*19.57)
-const TextButtons = parseInt(PixelRatio.getFontScale()*21.74);
-const ObjectHeigth = parseInt(PixelRatio.getFontScale()*38.04);
-const ImageHeigth = parseInt(PixelRatio.getFontScale()*434.78);
-const ImageWidth = parseInt(PixelRatio.getFontScale()*326.09);
-const TabNumber =parseInt(PixelRatio.getFontScale()*10.86)
+const IconosBottomTab = parseInt(PixelRatio.getFontScale()*30.43);//30 - 2
+const IconSelect = parseInt(PixelRatio.getFontScale()*19.57);//20 - 2
+const IconCamera = parseInt(PixelRatio.getFontScale()*52.17);//50 - 2
+const IconHeader = parseInt(PixelRatio.getFontScale()*41.30);//40 - 2
+const LabelBottomTab = parseInt(PixelRatio.getFontScale()*10.87);//12 - 2
+const TextoPantallas = parseInt(PixelRatio.getFontScale()*15.22);//16 - 2
+const TextoHeader = parseInt(PixelRatio.getFontScale()*17.39);//18 - 2
+const TextButtons = parseInt(PixelRatio.getFontScale()*19.57);//20 - 2
+const ObjectHeigth = parseInt(PixelRatio.getFontScale()*35.87);//35 - 2
+const ImageHeigth = parseInt(PixelRatio.getFontScale()*434.78);//400 - 2
+const ImageWidth = parseInt(PixelRatio.getFontScale()*326.09);//300 - 2
+const TabNumber =parseInt(PixelRatio.getFontScale()*8.7);//10 - 2
 
 
 
