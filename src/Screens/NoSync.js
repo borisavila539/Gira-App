c667a36e62f13e4517ee2a248512ad99a05368a0
borisavila539 <borisavila539@gmail.com>
2022-08-26 10:03:56 -0600

    ultimos cambios

--- src/Screens/NoSync.js
+++ src/Screens/NoSync.js
@@ -93,6 +93,7 @@ const NoSync = (props) => {
             setShowMensajeAlerta(true)
             setTipoMensaje(false)
         }
+        setIdSync('')
         setSincronizando(false)
         cantidadNoSync()
         historial()
